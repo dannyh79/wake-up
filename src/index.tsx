82c0eca3919cb5b6c553eec8c822d92c9b24ec12
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2023-08-09 16:03:07 +0800

    Set up PWA skeleton

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,16 @@ import { LocationProvider, Route, Router } from 'preact-iso';
 import Home from './pages/Home';
 import { NotFound } from './pages/_404.jsx';
 import './style.css';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    /* eslint-disable no-alert */
+    if (confirm('New content available. Reload?')) {
+      updateSW(true);
+    }
+  }
+});
 
 export function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,56 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 import eslint from 'vite-plugin-eslint';
 import path from 'path';
+import { VitePWA as vitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
+/* eslint-disable camelcase */
 export default defineConfig({
-  plugins: [preact(), eslint()],
+  plugins: [
+    preact(),
+    eslint(),
+    vitePWA({
+      includeAssets: ['**/*'],
+      manifest: {
+        background_color: '#ffffff',
+        description: 'Wakes your brain up with some quizzes',
+        display: 'standalone',
+        // icons: [
+        //   {
+        //     sizes: '192x192',
+        //     src: 'TODO-192x192.png',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     sizes: '512x512',
+        //     src: 'TODO-512x512.png',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     purpose: 'any',
+        //     sizes: '512x512',
+        //     src: 'TODO-512x512.png',
+        //     type: 'image/png',
+        //   },
+        //   {
+        //     purpose: 'maskable',
+        //     sizes: '512x512',
+        //     src: 'TODO-512x512.png',
+        //     type: 'image/png',
+        //   },
+        // ],
+        name: 'Wake Up!',
+        scope: '/',
+        short_name: 'WakeUp!',
+        start_url: '/',
+        theme_color: '#ffffff',
+      },
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*'],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
